9b3290c1df77d56e51a9125385ab5e6b079f33b6
Jihansaniyya <jihansaniyya2306@gmail.com>
2025-05-17 18:40:22 +0700

    Add files via upload

---
+++ Praktikum9 PBO/PersonDAO.java
@@ -0,0 +1,9 @@
+/**
+ * File: PersonDAO.java
+ * Deskripsi: Interface untuk person access object
+ */
+
+ public interface PersonDAO {
+ 
+    public void savePerson (Person p) throws Exception;
+ }
